280c1983144a9b68df6c3d7b311ee8e333bfd718
Nitin Gupta <niingupta910@gmail.com>
2020-03-20 13:09:27 -0700

    cleanup index.js

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,7 @@ import '@fortawesome/fontawesome-free/js/all'
 
 import katex from 'katex';
 import renderMathInElement from 'katex/dist/contrib/auto-render';
-//const katex = require('katex/dist/contrib/auto-render');
-//import('katex/dist/contrib/auto-render');
-//import * as ktx from 'katex/dist/contrib/auto-render';
-//console.log(katex);
-//console.log("================");
-//console.log(renderMathInElement);
+
 renderMathInElement(document.body, {
     delimiters: [
         {left: "$$", right: "$$", display: true},
